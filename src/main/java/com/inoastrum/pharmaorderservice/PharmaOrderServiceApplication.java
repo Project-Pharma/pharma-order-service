8867c972469a9c68525dab59886945299d7b9f55
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2020-08-10 08:57:04 -0700

    Initial Add

---
+++ src/main/java/com/inoastrum/pharmaorderservice/PharmaOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.inoastrum.pharmaorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PharmaOrderServiceApplication.class, args);
+    }
+
+}
